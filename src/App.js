5c07021acb4f0b9b3c721ecead46304ee4416bdc
lissetteag <lissette.almonteg@gmail.com>
2022-07-13 13:37:41 +0200

    changes visuals

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import { useState } from "react";
 import "./app.css";
 import TableRow from "./components/TableRow";
 import TableTitle from "./components/TableTitle";
+// import Hola from "./components/Hola";
 
 const attributes = [
   "password",
   "email",
-  "Phone Number",
+  "Phonenumber",
   "class",
-  "name",
-  "Phone Number",
-  "class",
-  "name",
+  "id",
+  "email",
+  "classes",
+  "color",
 ];
 const methods = [
   "study()",
   "can enroll()",
   "remove student()",
-  "read()",
-  "can enroll()",
-  "remove student()",
-  "read()",
+  "add activity()",
+  "write()",
+  "get student()",
+  "log out()",
 ];
 
 function App() {
@@ -68,34 +69,33 @@ function App() {
             <h3>Information</h3>
             <br />
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
-              quam est accusantium minus explicabo modi eaque voluptatum
-              molestiae! Dolores numquam tempore nam mollitia
+              The figure shows a class diagram with a class in blue. This class is the target class. 
+              The target class is the class subject to the recommendations. The classes shown in grey
+              are the context of the target class.
             </p>
             <br />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-              quisquam illo odio similique
+              Evaluate the recommended items presented in list to the right using the following metrics
             </p>
             <ul>
               <li>
-                <b>Relevant:</b> Lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
+                <b>Relevant:</b> The recommended item is relevant to the target class
               </li>
               <li>
-                <b>Obvious:</b> Lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
+                <b>Obvious:</b> The recommended item is too obvious for the target class
               </li>
               <li>
-                <b>Redundant:</b> Lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
+                <b>Redundant:</b> The recommended item exist or is similar to an existing one
               </li>
               <li>
-                <b>Context:</b> Lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
+                <b>Context:</b> The recommended item is relevant to target class and its context
+              </li>
+              <li>
+                <b>Specific:</b> The recommended item is specific to the target class
               </li>
             </ul>
           </div>
+          {/* <Hola name={"liss"}/> */}
         </div>
         <div className="rightBlock">
           <div>
@@ -122,7 +122,7 @@ function App() {
                 id=""
                 placeholder="Your recommendation"
               />
-              {page !== 0 && <button onClick={prevPage}>Previous</button>}
+              {page !== 0 && <button conClick={prevPage}>Previous</button>}
               {page !== 3 && <button onClick={nextPage}>Next</button>}
               {page === 3 && <button onClick={reset}>Submit</button>}
             </div>
---
+++ src/components/Hola.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/hola.css"
+
+const Hola = ({name}) => {
+  return (
+    <div className='hola'>Hola {name} </div>
+  )
+}
+
+export default Hola
